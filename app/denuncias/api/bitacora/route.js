1dd515c99a599dae30daa61b2a5cfb947d973b87
ArleAndino <arle.andino.reyes@gmail.com>
2025-11-09 23:35:54 -0600

    correcion

--- app/denuncias/api/bitacora/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import fs from "fs";
-import path from "path";
-
-export async function GET() {
-  const file = path.join(process.cwd(), "public/mockdata/bitacora.json");
-  const data = fs.readFileSync(file, "utf8");
-  return NextResponse.json(JSON.parse(data));
-}
